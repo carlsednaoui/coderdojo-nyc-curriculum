8d030b06658a0860dd4e7264f69c922af335188e
Justin Mitchell <justinmitchell@Justins-MacBook-Pro.local>
2014-03-30 20:43:13 -0400

    Adding Lesson 3, also some camelCasing

--- space-adventure/lesson-1/end-result/main.js
+++ space-adventure/lesson-1/end-result/main.js
@@ -1,16 +1,16 @@
-var ship_altitude = 0;
+var shipAltitude = 0;
 
 $('body').on('keydown',
 	function () {
 
 		// Then adjust the ship's altitude
-		ship_altitude = ship_altitude + 10;
+		shipAltitude = shipAltitude + 10;
 
 		// First we're going to change the image for the sprite
 		// Now we'll move the ship on the screen
 		$('#ship')
 			.attr('src', 'spaceship-moving.png')
-			.css('margin-bottom', ship_altitude)
+			.css('margin-bottom', shipAltitude)
 
 		// But since we aren't continuously firing the engines, let's
 		// reset the image after 500 milliseconds
