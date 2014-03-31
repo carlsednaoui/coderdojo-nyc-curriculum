ff5b2dfad2e7fb0527e67dc70236b660631ef9fc
Justin Mitchell <justinmitchell@Justins-MacBook-Pro.local>
2014-03-30 20:27:38 -0400

    Add lesson 2, gravity and ship movement

---
+++ space-adventure/lesson-2/end-result/main.js
@@ -0,0 +1,38 @@
+var ship_altitude = 400;
+var ship_velocity = 0;
+
+// These are constants you can use to fine-tune your gain.  Feel free to
+// try landing on a high-gravity planet by turning GRAVITY to 3 or 4.  Or
+// maybe you have a super powerful rocket with THRUST to 20
+var GRAVITY = 1;
+var THRUST = 10;
+
+var step = function () {
+	// First, we're going to update the altitude using our velocity
+	ship_altitude = ship_altitude + ship_velocity;
+	ship_velocity = ship_velocity - GRAVITY;
+
+	$('#ship').css('margin-bottom', ship_altitude)
+	window.setTimeout(step, 50);
+}
+
+step();
+
+$('body').on('keydown',
+	function () {
+
+		// We're shooting off the rocket, which is going to increase our velocity.
+		ship_velocity = ship_velocity + THRUST;
+
+		// Now, we want everyone to know we're firing the rockets, so let's update
+		// the image as before.
+		$('#ship').attr('src', 'spaceship-moving.png')
+
+		// But since we aren't continuously firing the engines, let's
+		// reset the image after 250 milliseconds
+		window.setTimeout(
+			function() {
+				$('#ship').attr('src', 'spaceship.png');
+			}, 
+			250);
+	});
