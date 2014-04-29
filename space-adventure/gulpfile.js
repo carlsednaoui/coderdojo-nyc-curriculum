923e906e307194af9a146b381b1e366bd36b23a3
carl sednaoui <carlsednaoui@gmail.com>
2014-04-29 01:00:40 -0400

    Update gulp file

--- space-adventure/gulpfile.js
+++ space-adventure/gulpfile.js
@@ -1,8 +1,30 @@
-var gulp  = require('gulp'),
+var fs    = require('fs'),
+    path  = require('path'),
+    gulp  = require('gulp'),
     zip   = require('gulp-zip'),
     clean = require('gulp-clean');
 
+function getFolders(dir){
+  return fs.readdirSync(dir)
+    .filter(function(file){
+      return fs.statSync(path.join(dir, file)).isDirectory();
+    });
+}
+
 gulp.task('default', function () {
-    gulp.src('lesson-*/end-result.zip', {read: false})
+  var folders = getFolders('./');
+
+  // remove the node_modules folder from the list of folders
+  var node_modules = folders.indexOf('node_modules');
+  folders.splice(node_modules,1);
+
+  folders.map(function(folder) {
+    gulp.src(folder + '/end-result.zip', {read: false})
       .pipe(clean());
+
+    gulp.src(folder + '/end-result/*')
+      .pipe(zip('end-result.zip'))
+      .pipe(gulp.dest(folder));
+  });
+
 });
